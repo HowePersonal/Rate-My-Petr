b522f4c33325bd6a66169c0d45c214c12eeb3e9a
Gabriel Gibb <gabrielgibb1@gmail.com>
2024-01-27 13:34:36 -0800

    added some functionality

--- static/search.js
+++ static/search.js
@@ -11,14 +11,31 @@ window.onload = function() {
 }
 
 
+async function verifyClassExists(department, num){
+    let url = 'https://api-next.peterportal.org/v1/rest/courses/' + department + num
+    const response = await fetch(url, {
+        method: 'GET'
+    });
+
+    data = await response.json()
+    console.log(data)
+
+    if (data.statusCode % 100 == 4){
+        alert("Class does not exist.")
+        return false;
+    } else {
+        window.location.href = `/review.html?department=${department}&number=${num}`;
+    }
+}
+
 function searchClass(e){
     let department = document.getElementById('menu').value;
     let num = document.getElementById('courseNumber').value;
     
     if (department.length < 1 || num.length < 1){
         alert("Please enter a department and course number.");
-    } else {
-        // Redirect to the review page with the parameters
-        window.location.href = `/review.html?department=${department}&number=${num}`;
-    }
+        return;
+    } 
+  
+    verifyClassExists(department, num);    
 }
