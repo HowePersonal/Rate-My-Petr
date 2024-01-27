8ea1c298304ec4790b9a57d5819a6954a5e0ebc2
Gabriel Gibb <gabrielgibb1@gmail.com>
2024-01-27 11:40:25 -0800

    app.py

--- app.py
+++ app.py
@@ -1,67 +1,35 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from requests import get
 import os
 from flask import render_template, request
 import time
 import database
-import re
 
 app = Flask(__name__)
 db = database.database()
 
-
-# def getClassById(id):
-#     try:
-#         course = get('https://api-next.peterportal.org/v1/rest/courses/' + id).json()['payload']
-#         return course
-#     except:
-#         return "Class Not Found"
-    
-def addArgsToUrl(url, args):
-    if args:
-        for key in args:
-            url += key + '=' + args[key] + '&'
-    return url
+CLASSES = {}
 
 @app.route('/')
 def hello():
-    return 'PETR'
+    return 'Hello, World! O'
 
+@app.route('/getClassById/<id>')
+def getClasses(id):
+    if CLASSES.get(id) is None:
+        return 'Class not found'
+    return CLASSES[id]
 
-@app.route('/api/getClass')
-def getClasses():
-    url = 'https://api-next.peterportal.org/v1/rest/courses?'
-    url = addArgsToUrl(url, request.args)
-    return get(url).json()['payload']
+def setClasses():
+    classes = get('https://api.peterportal.org/rest/v0/courses/all').json()
+    for i in range(len(classes)):
+        CLASSES[classes[i]['id']] = classes[i]
 
-
-@app.route('/api/getProf/<ucinetid>')
-def getProfs(ucinetid):
-    url = 'https://api-next.peterportal.org/v1/rest/instructors/' + ucinetid
-    # url = addArgsToUrl(url, request.args)
-    return get(url).json()['payload']
-
-
-@app.route('/api/getGradesFromClass')
-def getGrades():
-    url = 'https://api-next.peterportal.org/v1/rest/grades/aggregate?'
-    url = addArgsToUrl(url, request.args)
-    
-    return get(url).json()['payload']
-
-# @app.route('/api/getProfsFromClassId/<id>')
-# def getProfs(id):
-#     profsList = []
-#     classInfo = getClassById(id)
-#     print(classInfo)
-#     for i in range(len(classInfo['instructors'])):
-#         profsList.append(get('https://api-next.peterportal.org/v1/rest/instructors/' + classInfo['instructors'][i]['ucinetid']).json())
-    
-#     profsWithoutPayload = []
-#     for prof in profsList:
-#         profsWithoutPayload.append(prof['payload'])
-#     return profsWithoutPayload
-
+#SERVER HTML FILES
+@app.route('/<path:filename>')
+def serve_frontend(filename):
+    print(filename)
+    return render_template(filename)
 
 @app.route('/api/insertRating', methods=['POST'])
 def insertRating():
@@ -72,7 +40,6 @@ def insertRating():
         difficulty_rating = data['difficultyRating']
         comment = data['comment']
         grade = data['grade']
-        added_timestamp = time.time()
         instructor_id = data['instructorId']
         db.insertRating(class_id, str(enjoyment_rating), str(difficulty_rating), comment, grade, instructor_id)
         
@@ -106,34 +73,14 @@ def updateRating():
     except Exception as e:
         return "Unsuccessful update"
 
-# @app.route('/api/getClassRatings/<classId>', methods=['GET'])
-# def getRatings(classId):
-#     try:
-        
-        
-#     except Exception as e:
-#         return "Unsuccessful when trying to get ratings"
-    
-# TODO:
-# Fix this function
-# Get ratings from course ID
-# Optionally we might want to get ratings from intructor ucinetid
-@app.route('/api/getRatings')
-def getRatings():
+@app.route('/api/getClassRatings/<classId>', methods=['GET'])
+def getRatings(classId):
     try:
-        data = request.get_json()
-        class_id = data['classId']
-        # return db.getRatings(class_id)
-    except:
-        return "Unsuccessful retrieval"
+        rating_records = db.getClassRatings(classId)
+        return rating_records
+    except Exception as e:
+        return "Unsuccessful when trying to get ratings"
     
 
-
-
-#SERVE HTML
-@app.route('/<path:filename>')
-def serve_frontend(filename):
-    try:
-        return render_template(filename)
-    except:
-        return "PATH NOT FOUND"
+#ACT LIKE THIS IS THE MAIN FUNCTION DW ABOUT IT
+# setClasses()
