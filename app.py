c5e664b340166129f16f345c5e5491d09bf407d8
Gabriel Gibb <gabrielgibb1@gmail.com>
2024-01-26 19:41:15 -0800

    update

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, send_from_directory
+from requests import get
+import os
+from flask import render_template
+from flask import render_template
+
+app = Flask(__name__)
+
+CLASSES = {}
+
+@app.route('/')
+def hello():
+    return 'Hello, World! O'
+
+@app.route('/getClassById/<id>')
+def getClasses(id):
+    if CLASSES.get(id) is None:
+        return 'Class not found'
+    return CLASSES[id]
+
+def setClasses():
+    classes = get('https://api.peterportal.org/rest/v0/courses/all').json()
+    for i in range(100):
+        CLASSES[classes[i]['id']] = classes[i]
+
+setClasses()
+
+@app.route('/frontend/<path:filename>')
+def serve_frontend(filename):
+    print(filename)
+    return render_template(filename)
+
+if __name__ == '__main__':
+    app.run()
