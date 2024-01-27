ca93bf9eb0a5e73795795c5cd0f1a74098cbd4f9
Gabriel Gibb <gabrielgibb1@gmail.com>
2024-01-26 21:27:42 -0800

    update

--- app.py
+++ app.py
@@ -11,22 +11,39 @@ CLASSES = {}
 def hello():
     return 'Hello, World! O'
 
+# TODO:
+# Get this info from api, dont store it
 @app.route('/getClassById/<id>')
 def getClasses(id):
     if CLASSES.get(id) is None:
         return 'Class not found'
     return CLASSES[id]
 
-#SERVER HTML FILES
+@app.route('/getProfessorsByClassId/<id>')
+def getProfessors(id):
+    if CLASSES.get(id) is None:
+        return 'Class not found'
+    print(CLASSES[id]['professor_history'])
+    profs = []
+    for i in range(len(CLASSES[id]['professor_history'])):
+        profs.append(get('https://api.peterportal.org/rest/v0/instructors/' + CLASSES[id]['professor_history'][i]).json())
+    return profs
+
+
+
+#SERVE HTML
 @app.route('/<path:filename>')
 def serve_frontend(filename):
     print(filename)
     return render_template(filename)
 
+
+#MAIN FUNCTION
 def setClasses():
     classes = get('https://api.peterportal.org/rest/v0/courses/all').json()
     for i in range(len(classes)):
         CLASSES[classes[i]['id']] = classes[i]
+
 
 #ACT LIKE THIS IS THE MAIN FUNCTION DW ABOUT IT
 setClasses()
